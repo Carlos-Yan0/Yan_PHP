8ad078dc8cfb25173fad9df1babec906570bdae9
Yan Carlos <yan.c.oliveira08@gmail.com>
2025-07-30 17:00:11 -0300

    Menu responsavel pela navegação
    optei por criar um arquivo de menu pois posso apenas dar um include

---
+++ conexao/conexaoPdo/menu.php
@@ -0,0 +1,19 @@
+<header>
+        <nav>
+            <div>
+                <a href="index.php">Home</a>
+            </div>
+            <div>
+                <a href="listarClientes.php">Listar CLientes</a>
+            </div>
+            <div>
+                <a href="atualizarCliente.php">Atualizar CLientes</a>
+            </div>
+            <div>
+                <a href="deletarCliente.php">Deletar CLientes</a>
+            </div>
+            <div>
+                <a href="inserirCliente.php">Inserir CLientes</a>
+            </div>
+        </nav>
+    </header>
