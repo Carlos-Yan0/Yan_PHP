7334d26b9f1f023bec2f564e29ed30c86e641072
Yan Carlos <yan.c.oliveira08@gmail.com>
2025-07-30 17:01:59 -0300

    Mudanças para deixar o codigo mas bonito
    Coloquei o codigo dentro de divs e as atribui classes para deixar o codigo mais semantico

--- conexao/conexaoPdo/atualizarCliente.php
+++ conexao/conexaoPdo/atualizarCliente.php
@@ -33,6 +33,11 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Atualizar Cliente</title>
+    <!-- Link do Bootstrap -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-LN+7fdVzj6u52u30Kp6M/trliBMCMKTyK833zpbD+pXdCLuTusPj697FH4R/5mcr" crossorigin="anonymous">
+    <!-- Link do CSS -->
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/atualizarClientes.css">
     <script>
         function habilitarEdicao(campo){
             document.getElementById(campo).removeAttribute("readonly");
@@ -40,34 +45,54 @@
     </script>
 </head>
 <body>
-    <h2>Atualizar Cliente</h2>
-    <!-- Se houver erro exibe a msg e o campo de busca -->
-    <?php if($msgErro): ?>
-        <p style="color:red;"><?=htmlspecialchars($msgErro)?></p>
-    <form action="atualizarCliente.php" method="GET">
-    <label for="id">ID do Cliente:</label>
-    <input type="number" id="id" name="id" required>
-    <button type="submit">Buscar</button>
-    </form>
-    <?php else: ?>
-        <!-- Se um cliente foi encontrado, exibe o formulario preenchido -->
-         <form action="processarAtualizacao.php" method="POST">
-            <input type="hidden" name="id_cliente" value="<?=htmlspecialchars($cliente['ID_cliente'])?>">
 
-            <label for="nome">Nome:</label>
-            <input type="text" id="nome" name="nome" value="<?=htmlspecialchars($cliente['nome'])?>" readonly onclick="habilitarEdicao('nome')">
+        <?php include 'menu.php'?>
 
-            <label for="endereco">Endereço:</label>
-            <input type="text" id="endereco" name="endereco" value="<?=htmlspecialchars($cliente['endereco'])?>" readonly onclick="habilitarEdicao('endereco')">
 
-            <label for="telefone">Telefone:</label>
-            <input type="text" id="telefone" name="telefone" value="<?=htmlspecialchars($cliente['telefone'])?>" readonly onclick="habilitarEdicao('telefone')">
+    <div class="container">
+        <h2>Atualizar Cliente</h2>
+        <!-- Se houver erro exibe a msg e o campo de busca -->
+        <?php if($msgErro): ?>
+            <p style="color:red;"><?=htmlspecialchars($msgErro)?></p>
+        <form action="atualizarCliente.php" method="GET">
+        <label for="id">ID do Cliente:</label>
+            <div class="inserirID">  
+                <input type="number" id="id" name="id" required class="form-control">
+                <button type="submit" class="btn btn-success">Buscar</button>
+            </div>
+        
+        </form>
+    
 
-            <label for="email">E-mail:</label>
-            <input type="email" id="email" name="email" value="<?=htmlspecialchars($cliente['email'])?>" readonly onclick="habilitarEdicao('email')">
 
-            <button type="submit">Atualizar Cliente</button>
-         </form>
-         <?php endif; ?>
+        <?php else: ?>
+                <!-- Se um cliente foi encontrado, exibe o formulario preenchido -->
+                <form action="processarAtualizacao.php" method="POST" class="form">
+                    <input type="hidden" name="id_cliente" value="<?=htmlspecialchars($cliente['ID_cliente'])?>">
+
+                    <label for="nome">Nome:</label>
+                    <input type="text" id="nome" name="nome" value="<?=htmlspecialchars($cliente['nome'])?>" readonly onclick="habilitarEdicao('nome')" class="form-control">
+
+                    <label for="endereco">Endereço:</label>
+                    <input type="text" id="endereco" name="endereco" value="<?=htmlspecialchars($cliente['endereco'])?>" readonly onclick="habilitarEdicao('endereco')" class="form-control">
+
+                    <label for="telefone">Telefone:</label>
+                    <input type="text" id="telefone" name="telefone" value="<?=htmlspecialchars($cliente['telefone'])?>" readonly onclick="habilitarEdicao('telefone')" class="form-control">
+
+                    <label for="email">E-mail:</label>
+                    <input type="email" id="email" name="email" value="<?=htmlspecialchars($cliente['email'])?>" readonly onclick="habilitarEdicao('email')" class="form-control">
+
+                    <button type="submit" class="btn btn-success">Atualizar Cliente</button>
+                </form>
+            <?php endif; ?>
+    </div>
+
+    <?php include 'toast.php'?>
+
+    <footer>
+        <address>
+                Yan Carlos de oliveira - Estudante SesiSenai - Desenvolvimento de Sistemas
+        </address>
+    </footer>
 </body>
 </html>
